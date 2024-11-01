b86f177aeb34a17cb47682129808838dfeadb725
HanilJain <shirshjain0111@gmail.com>
2024-11-01 22:24:44 +0530

    added screenhot and removed from

--- event.js
+++ event.js
@@ -1,29 +1,3 @@
-// document.getElementById("upload-form").addEventListener("submit", function(event) {
-//     event.preventDefault();
-//     const formData = new FormData();
-//     formData.append("url", document.getElementById("url").value);
-//     formData.append("description", document.getElementById("bugTitle").value);
-//     formData.append("label" , document.getElementById("labelSelect").value );
-//     formData.append("Markdown description" , document.getElementById("description").value);
-//     formData.append("screenshots" , document.getElementById("image-upload").files[0]);
-
-
-//     fetch("http://localhost:8000/api/v1/issues/", {
-//         method: "POST",
-//         body: FormData
-//     })
-//     .then(response => response.json())
-//     .then(data => {
-//         console.log("Issue created successfully:", data);
-//         alert("Issue reported successfully!");
-//     })
-//     .catch(error => {
-//         console.error("Error creating issue:", error);
-//         alert("Error submitting the issue. Please try again.");
-//     });
-
-// });
-
 function cancelForm() {
     // Clear the form
     document.getElementById("upload-form").reset();
